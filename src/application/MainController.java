ff9722e8eb552fd22cc89bab5495eb62a1f0e659
Eldar <eldar.haseljic@fet.ba>
2019-08-05 18:23:45 +0200

    Log in na bazu

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,5 +1,16 @@
 package application;
 
+import java.util.List;
+
+import javax.persistence.Query;
+
+import entiteti.Korisnik;
+import entiteti.Student;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -23,21 +34,44 @@ public class MainController {
 	
 	public void Login(ActionEvent event) throws Exception {
 		
-		if(txtUsername.getText().equals("user") && txtPassword.getText().equals("pass"))
+		boolean exists = false;
+		
+		String usernameField = txtUsername.getText();
+		String passwordField = txtPassword.getText();
+		
+		String PERSISTENCE_UNIT_NAME = "raspored";
+		EntityManagerFactory emf;
+		emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+		EntityManager em = emf.createEntityManager();
+		
+		Query q = em.createQuery("SELECT k FROM Student k");
+		List<Student> korisnici = q.getResultList();
+		
+		for(Student k : korisnici)
 		{
-			Parent root = FXMLLoader.load(getClass().getResource("ToDo.fxml"));
-			Scene scene = new Scene(root);
-			Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-			primaryStage.setScene(scene);
-			//primaryStage.setResizable(true);
-			primaryStage.show();
+			String username = k.getImeStud();
+			String password = k.getPrezStud();
+			
+			if(usernameField.equals(username) && passwordField.equals(password))
+			{
+				exists = true;
+				Parent root = FXMLLoader.load(getClass().getResource("ToDo.fxml"));
+				Scene scene = new Scene(root);
+				Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+				primaryStage.setScene(scene);
+				//primaryStage.setResizable(true);
+				primaryStage.show();
+			}
+			
 		}
-		else
+		
+		if(!exists)
 		{
 			txtUsername.clear();
 			txtPassword.clear();
 			lblStatus.setText("Login failed, please try again");
 		}
+		
 	}
 	
 }
