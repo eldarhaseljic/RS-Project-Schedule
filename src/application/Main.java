c73ebae1ea1d96729c6007cdabb6136ad2af2aa4
mahirBajramovic <mahir.bajramovic@fet.ba>
2019-08-06 15:10:53 +0200

    New login UI

--- src/application/Main.java
+++ src/application/Main.java
@@ -17,6 +17,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.setResizable(false);
+			primaryStage.setTitle("Login Screen");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
